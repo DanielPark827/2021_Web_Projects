9f3233a04d1fcf2cd41e434b34f880d30415452e
CoffeeGuy827 <psh01312@naver.com>
2021-06-21 02:47:09 +0900

    Daniel

---
+++ src/main/java/kr/ac/inha/board/controller/BoardController.java
@@ -0,0 +1,13 @@
+package kr.ac.inha.board.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+}
